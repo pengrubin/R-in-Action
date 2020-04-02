7af2af1e0525bd89b32f246491c973dbf1d9ca9f
Hongwei <pengrubin@foxmail.com>
2020-04-03 02:27:25 +0800

    Update Ch02 Creating a dataset.R
    创建矩阵和数组

--- Ch02 Creating a dataset.R
+++ Ch02 Creating a dataset.R
@@ -25,6 +25,7 @@ a[2:6]
 # Listing 2.1 - Creating Matrices
 y <- matrix(1:20, nrow=5, ncol=4)
 y
+#建立矩阵
 cells    <- c(1,26,24,68)
 rnames   <- c("R1", "R2")
 cnames   <- c("C1", "C2") 
@@ -49,6 +50,7 @@ x[1, c(4,5)]
 dim1 <- c("A1", "A2")
 dim2 <- c("B1", "B2", "B3")
 dim3 <- c("C1", "C2", "C3", "C4")
+#建立数组
 z <- array(1:24, c(2,3,4), dimnames=list(dim1, dim2, dim3))
 z
 
